d08de5156ec9c98f50c94e6535bbed2eea3ec0db
ManciuAna <manciu_anamaria@yahoo.ro>
2024-10-25 18:04:53 +0100

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const itemsUl = document.querySelector(".gallery");
+galleryItems.forEach(item => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("gallery__item");
+    listItem.innerHTML =
+        `
+    <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+   `;
+    itemsUl.append(listItem);
+});
+
+itemsUl.addEventListener("click", openImageInModal);
+
+function openImageInModal(event) {
+    event.preventDefault();
+    const lightBox = new SimpleLightbox(".gallery a", {
+        captionPosition: "bottom",
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+}
